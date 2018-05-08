bb5b447691613a5d20f2c3f5bb69546eec395da1
Tomas Havlicek <tomas.havlicek@quanti.cz>
2018-05-08 11:45:47 +0200

    FEAT: moved app secret definition to properties file

--- src/main/java/cz/cvut/fit/havlito4/notification_server/controller/NotificationController.java
+++ src/main/java/cz/cvut/fit/havlito4/notification_server/controller/NotificationController.java
@@ -4,6 +4,7 @@ import cz.cvut.fit.havlito4.notification_server.controller.entity.NotificationRe
 import cz.cvut.fit.havlito4.notification_server.service.NotificationService;
 import cz.cvut.fit.havlito4.notification_server.service.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,7 +13,8 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping(value = "/notification", produces = "application/json")
 public class NotificationController {
 
-    private static final String classificationAuthCode = "abc";
+    @Value("${notification_server.appserver.secret}")
+    private String classificationAuthCode;
 
     @Autowired
     private NotificationService notificationService;
